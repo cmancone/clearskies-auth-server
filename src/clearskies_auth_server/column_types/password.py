2cc6c224d92fdd78a39610ee35c23e523a036e84
Conor Mancone <cmancone@gmail.com>
2023-07-12 06:41:57 -0400

    WIP

--- src/clearskies_auth_server/column_types/password.py
+++ src/clearskies_auth_server/column_types/password.py
@@ -1,4 +1,5 @@
-from clearskies.column_types.string import String
+from clearskies.column_types import string, String
+from clearskies.input_requirements import required
 from passlib.context import CryptContext
 class Password(String):
     _crypt_context = None
@@ -7,6 +8,9 @@ class Password(String):
         "crypt_context",
         "crypt_context_string",
         "crypt_context_path",
+        "require_repeat_password",
+        "repeat_password_column_name",
+        "for_login",
     ]
 
     crypt_config_names = [
@@ -34,6 +38,13 @@ class Password(String):
                 "you can only provide one of 'crypt_context', 'crypt_context_string', and 'crypt_context_path', " +
                 "but more than one was found"
             )
+        repeat_password_column_name = configuration.get("repeat_password_column_name")
+        if not isinstance(repeat_password_column_name, str):
+            raise ValueError(
+                f"Error for column '{self.name}' in model '{self.model_class.__name__}': " +
+                "repeat_password_column_name should be a string, but instead I received a " +
+                type(repeat_password_column_name)
+            )
 
     def _finalize_configuration(self, configuration):
         configuration = super()._finalize_configuration(configuration)
@@ -58,6 +69,14 @@ class Password(String):
         else:
             self._crypt_context = CryptContext.from_path(self.config("crypt_context_path"))
 
+    def check_input(self, model, data):
+        if self.name not in data or not data[self.name]:
+            return ""
+        # if we're providing a password, then it must match the value in 'repeat_password'
+        repeat_password = data.get("repeat_password")
+        password = data.get("password")
+        return "Passwords did not match" if password != repeat_password else ""
+
     def pre_save(self, data, model):
         # if the password is being set to a non-value, then unset it
         if self.name in data and not data[self.name]:
@@ -81,3 +100,12 @@ class Password(String):
         if self._crypt_context.needs_update(hashed_password):
             user.save({self.name: password})
         return True
+
+    def additional_write_columns(self, is_create=False):
+        if self.config("for_login"):
+            return {}
+
+        extra_columns = super().additional_write_columns(is_create=is_create)
+        if self.config("require_repeat_password"):
+            extra_columns.update([string("repeat_password", is_temporary=True)])
+        return extra_columns
